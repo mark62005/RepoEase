a68b93c4165edbf2f110d7e07ddc8fcef04b754d
Mark Wong <markwong.digital@gmail.com>
2024-03-12 01:57:36 -0700

    Set up backend project with Spring Boot

---
+++ src/main/java/com/markwongse/RepoEase/RepoEaseApplication.java
@@ -0,0 +1,13 @@
+package com.markwongse.RepoEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoEaseApplication.class, args);
+	}
+
+}
